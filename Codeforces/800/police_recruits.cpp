36a45b4c59708520a6b98299d2ba0390ce6512b1
Mahim <mahim@Mahims-MacBook-Air.local>
2026-07-27 21:05:42 +0600

    Codeforces - Police Recruits

---
+++ Codeforces/800/police_recruits.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int m[n];
+    int police = 0;
+    int crime = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> m[i];
+        if (m[i] == -1)
+        {
+            if (police == 0)
+            {
+                crime++;
+            }
+            else
+            {
+                police--;
+            }
+        }
+        else
+        {
+            police = police + m[i];
+        }
+    }
+    cout << crime << endl;
+    return 0;
+}
